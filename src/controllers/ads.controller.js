7299a89b9af822c01bdf429f05e8cdf893593b0e
IDStack <ahmadhakim404@gmail.com>
2021-07-01 00:47:03 +0700

    refactor create ads data

--- src/controllers/ads.controller.js
+++ src/controllers/ads.controller.js
@@ -25,23 +25,12 @@ exports.create = (req, res) => {
     return
   }
 
-  // Create an Ads
   const ads = {
+    ...req.body,
     user_id: req.userId,
-    title: req.body.title,
-    category_id: req.body.category_id,
-    brand: req.body.brand,
-    model: req.body.model,
-    year: req.body.year,
-    price: req.body.price,
-    description: req.body.description,
-    address: req.body.address,
-    location_lat: req.body.location_lat,
-    location_long: req.body.location_long,
-    sold: req.body.sold ? req.body.sold : false,
   }
 
-  // Save Ads in the database
+  //   Save Ads in the database
   Ads.create(ads)
     .then((data) => {
       res.send(data)
